7e4e49de776bdf59fb0e8f5df68a4886fa3e1efa
Daniel Lopez <d@niel.to>
2025-07-07 16:45:07 -0700

    initial commit

---
+++ src/lib/common.ts
@@ -0,0 +1,27 @@
+export const DEFAULT_TITLE = 'hi my name is daniel'
+
+export const nav = [
+  {
+    url: '/',
+    text: 'home',
+  },
+  {
+    url: '/resume',
+    text: 'resume',
+  },
+  {
+    url: '/portfolio',
+    text: 'portfolio',
+  },
+]
+
+export const socials = [
+  {
+    name: 'linkedin',
+    url: 'https://www.linkedin.com/in/d4niellopez',
+  },
+  {
+    name: 'github',
+    url: 'https://github.com/b4conjuice',
+  },
+]
