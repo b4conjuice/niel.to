acd3e7576f872abe36967a81eefc1466dc3f0ba4
Daniel Lopez <d@niel.to>
2025-07-07 22:04:42 -0700

    refactor(home): rebuild homepage using astro template instead react

--- src/lib/common.ts
+++ src/lib/common.ts
@@ -1,3 +1,5 @@
+import { GithubIcon, LinkedInIcon } from '../components/icons'
+
 export const DEFAULT_TITLE = 'hi my name is daniel'
 
 export const nav = [
@@ -25,3 +27,8 @@ export const socials = [
     url: 'https://github.com/b4conjuice',
   },
 ]
+
+export const socialIcons: Record<string, () => React.JSX.Element> = {
+  linkedin: LinkedInIcon,
+  github: GithubIcon,
+}
